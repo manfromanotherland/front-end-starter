79e088e8ee9d0c6c2d79015f71d132237b57ff9a
Edmundo Junior <hello@edmundojr.com>
2014-11-03 22:44:17 +0000

    Update body class and viewport meta tag

--- inc/header.php
+++ inc/header.php
@@ -17,9 +17,9 @@
 		Title
 	</title>
 	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css">
+	<link rel="stylesheet" href="assets/css/normalize.css">
 	<link rel="stylesheet" href="assets/css/main.css">
 
 	<!--[if lt IE 9]>
@@ -28,4 +28,4 @@
 
 </head>
 
-<body class="<?php echo $page; ?>">
+<body class="<?php echo "is-$page"; ?>">
