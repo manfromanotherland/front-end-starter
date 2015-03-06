584a2254875cbea837a29dc493142d3a074eb1b2
Edmundo Junior <hello@edmundojr.com>
2015-03-06 18:12:03 +0000

    Update jQuery to version 1.11.2

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.0.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.2.min.js"><\/script>')</script>
 	<script src="assets/js/main.js"></script>
 
 	<!-- Google Analytics -->
